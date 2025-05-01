5ec65a28eb4020d797f80abe6c58e0465c1648f8
Anna-Nielepiec <120506261+Anna-Nielepiec@users.noreply.github.com>
2025-05-01 17:48:56 +0200

    Add files via upload

---
+++ cmsms/tmp/templates_c/cmsms_parser_0_52861600_1745094913^9ba91d3539a9fdecb1ac7b80c4819c93c03e1bed_1.cms_template.14.php
@@ -0,0 +1,42 @@
+<?php
+/* Smarty version 4.5.2, created on 2025-04-19 22:35:13
+  from 'cms_template:14' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.2',
+  'unifunc' => 'content_6804090182acf7_39288581',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '9ba91d3539a9fdecb1ac7b80c4819c93c03e1bed' => 
+    array (
+      0 => 'cms_template:14',
+      1 => '1745085193',
+      2 => 'cms_template',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6804090182acf7_39288581 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_checkPlugins(array(0=>array('file'=>'/Applications/XAMPP/xamppfiles/htdocs/cmsms/lib/plugins/function.global_content.php','function'=>'smarty_function_global_content',),1=>array('file'=>'/Applications/XAMPP/xamppfiles/htdocs/cmsms/lib/plugins/function.sitename.php','function'=>'smarty_function_sitename',),2=>array('file'=>'/Applications/XAMPP/xamppfiles/htdocs/cmsms/lib/plugins/function.title.php','function'=>'smarty_function_title',),));
+echo smarty_function_global_content(array('name'=>'a_part_top'),$_smarty_tpl);?>
+
+
+	<header id="header">
+		<h1><?php echo smarty_function_sitename(array(),$_smarty_tpl);?>
+</h1>
+	</header>
+
+
+	<section id="content">
+		<h1><?php echo smarty_function_title(array(),$_smarty_tpl);?>
+</h1>
+		
+	</section>
+
+<?php echo smarty_function_global_content(array('name'=>'a_part_bottom'),$_smarty_tpl);
+}
+}
