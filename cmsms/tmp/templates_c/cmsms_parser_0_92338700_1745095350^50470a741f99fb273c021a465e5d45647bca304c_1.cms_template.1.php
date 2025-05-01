0854fc3d04e0e1068eb23091e870aeaac23877a7
Anna-Nielepiec <120506261+Anna-Nielepiec@users.noreply.github.com>
2025-05-01 17:53:59 +0200

    Add files via upload

---
+++ cmsms/tmp/templates_c/cmsms_parser_0_92338700_1745095350^50470a741f99fb273c021a465e5d45647bca304c_1.cms_template.1.php
@@ -0,0 +1,64 @@
+<?php
+/* Smarty version 4.5.2, created on 2025-04-19 22:42:30
+  from 'cms_template:1' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.2',
+  'unifunc' => 'content_68040ab6e18593_83241375',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '50470a741f99fb273c021a465e5d45647bca304c' => 
+    array (
+      0 => 'cms_template:1',
+      1 => '1745094730',
+      2 => 'cms_template',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_68040ab6e18593_83241375 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_checkPlugins(array(0=>array('file'=>'/Applications/XAMPP/xamppfiles/htdocs/cmsms/lib/plugins/function.global_content.php','function'=>'smarty_function_global_content',),));
+?>
+<!DOCTYPE HTML>
+<!--
+	Introspect by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+--><html><head><title>Introspect by TEMPLATED</title><meta charset="utf-8"><meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"><meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"><link rel="stylesheet" href="assets/css/main.css"></head><body>
+
+		<!-- Header -->
+			<?php echo smarty_function_global_content(array('name'=>"Template-Header"),$_smarty_tpl);?>
+
+
+		<!-- Banner -->
+			<?php echo smarty_function_global_content(array('name'=>"Template-Banner"),$_smarty_tpl);?>
+
+          
+                  <!-- One -->
+                       <?php echo smarty_function_global_content(array('name'=>"Template-One"),$_smarty_tpl);?>
+
+ 
+                    <!-- Two -->
+                         <?php echo smarty_function_global_content(array('name'=>"Template-Two"),$_smarty_tpl);?>
+
+			
+		      <!-- Footer -->
+			 <?php echo smarty_function_global_content(array('name'=>"Template-Footer"),$_smarty_tpl);?>
+
+
+		<!-- Scripts -->
+			<?php echo '<script'; ?>
+ src="assets/js/jquery.min.js"><?php echo '</script'; ?>
+><?php echo '<script'; ?>
+ src="assets/js/skel.min.js"><?php echo '</script'; ?>
+><?php echo '<script'; ?>
+ src="assets/js/util.js"><?php echo '</script'; ?>
+> 
+                        <?php echo '<script'; ?>
+ src="assets/js/main.js"><?php echo '</script'; ?>
+></body></html><?php }
+}
