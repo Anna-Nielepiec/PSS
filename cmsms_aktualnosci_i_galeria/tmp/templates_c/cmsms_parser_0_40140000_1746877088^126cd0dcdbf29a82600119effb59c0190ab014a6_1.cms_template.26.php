b6d32c7efdfe3c3912fa733e1df44785d3d7c00f
Anna-Nielepiec <120506261+Anna-Nielepiec@users.noreply.github.com>
2025-05-13 20:18:14 +0200

    Add files via upload

---
+++ cmsms_aktualnosci_i_galeria/tmp/templates_c/cmsms_parser_0_40140000_1746877088^126cd0dcdbf29a82600119effb59c0190ab014a6_1.cms_template.26.php
@@ -0,0 +1,141 @@
+<?php
+/* Smarty version 4.5.2, created on 2025-05-10 13:38:08
+  from 'cms_template:26' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.2',
+  'unifunc' => 'content_681f3aa0621527_89895157',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '126cd0dcdbf29a82600119effb59c0190ab014a6' => 
+    array (
+      0 => 'cms_template:26',
+      1 => '1746723124',
+      2 => 'cms_template',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_681f3aa0621527_89895157 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_checkPlugins(array(0=>array('file'=>'/Applications/XAMPP/xamppfiles/htdocs/cmsms/lib/plugins/function.global_content.php','function'=>'smarty_function_global_content',),));
+?>
+<!DOCTYPE HTML>
+<html lang="pl">
+  <head>
+    <title>Elements - Introspect by TEMPLATED</title>
+    <meta charset="utf-8">
+    <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+   
+    
+  </head>
+  <body>
+
+    <!-- Header -->
+    <?php echo smarty_function_global_content(array('name'=>"Template-Header"),$_smarty_tpl);?>
+
+
+    <!-- Gallery Styles and Scripts -->
+    <link rel="stylesheet" href="/cmsms/html5up-multiverse/assets/css/main.css" />
+    <link rel="stylesheet" href="/cmsms/template/assets/css/main.css">
+    <link rel="stylesheet" href="/cmsms/html5up-multiverse/assets/css/fontawesome.css" />
+    <?php echo '<script'; ?>
+ src="/cmsms/html5up-multiverse/assets/js/jquery.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="/cmsms/html5up-multiverse/assets/js/main.js"><?php echo '</script'; ?>
+>
+
+    <!-- Gallery Section -->
+    <div id="main">
+      <article class="thumb">
+        <a href="/cmsms/html5up-multiverse/images/fulls/pic03.jpg" class="image">
+          <img src="/cmsms/html5up-multiverse/images/thumbs/pic03.jpg" alt="" />
+        </a>
+        <h2>Góry_1</h2>
+        <p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
+      </article>
+      <article class="thumb">
+        <a href="/cmsms/html5up-multiverse/images/fulls/pic04.jpg" class="image">
+          <img src="/cmsms/html5up-multiverse/images/thumbs/pic04.jpg" alt="" />
+        </a>
+        <h2>Góry_2</h2>
+        <p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
+      </article>
+      <article class="thumb">
+        <a href="/cmsms/html5up-multiverse/images/fulls/pic05.jpg" class="image">
+          <img src="/cmsms/html5up-multiverse/images/thumbs/pic05.jpg" alt="" />
+        </a>
+        <h2>Góry_3</h2>
+        <p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
+      </article>
+      <article class="thumb">
+        <a href="/cmsms/html5up-multiverse/images/fulls/pic06.jpg" class="image">
+          <img src="/cmsms/html5up-multiverse/images/thumbs/pic06.jpg" alt="" />
+        </a>
+        <h2>Góry_4</h2>
+        <p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
+      </article>
+      <article class="thumb">
+        <a href="/cmsms/html5up-multiverse/images/fulls/pic07.jpg" class="image">
+          <img src="/cmsms/html5up-multiverse/images/thumbs/pic07.jpg" alt="" />
+        </a>
+        <h2>Góry_5</h2>
+        <p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
+      </article>
+      <article class="thumb">
+        <a href="/cmsms/html5up-multiverse/images/fulls/pic08.jpg" class="image">
+          <img src="/cmsms/html5up-multiverse/images/thumbs/pic08.jpg" alt="" />
+        </a>
+        <h2>Góry_6</h2>
+        <p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
+      </article>
+      <article class="thumb">
+        <a href="/cmsms/html5up-multiverse/images/fulls/pic09.jpg" class="image">
+          <img src="/cmsms/html5up-multiverse/images/thumbs/pic09.jpg" alt="" />
+        </a>
+        <h2>Góry_7</h2>
+        <p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
+      </article>
+      <article class="thumb">
+        <a href="/cmsms/html5up-multiverse/images/fulls/pic10.jpg" class="image">
+          <img src="/cmsms/html5up-multiverse/images/thumbs/pic10.jpg" alt="" />
+        </a>
+        <h2>Góry_8</h2>
+        <p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
+      </article>
+      <article class="thumb">
+        <a href="/cmsms/html5up-multiverse/images/fulls/pic11.jpg" class="image">
+          <img src="/cmsms/html5up-multiverse/images/thumbs/pic11.jpg" alt="" />
+        </a>
+        <h2>Góry_9</h2>
+        <p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
+      </article>
+    </div>
+
+    <!-- Footer -->
+    <?php echo smarty_function_global_content(array('name'=>"Template-Footer_down"),$_smarty_tpl);?>
+
+
+    <!-- Scripts: tylko dla szablonu -->
+    <?php echo '<script'; ?>
+ src="/cmsms/template/assets/js/jquery.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="/cmsms/template/assets/js/skel.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="/cmsms/template/assets/js/util.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="/cmsms/template/assets/js/main.js"><?php echo '</script'; ?>
+>
+
+  </body>
+</html><?php }
+}
---
+++ cmsms_aktualnosci_i_galeria/tmp/templates_c/cmsms_parser_0_43927800_1745147539^50470a741f99fb273c021a465e5d45647bca304c_1.cms_template.1.php
@@ -0,0 +1,74 @@
+<?php
+/* Smarty version 4.5.2, created on 2025-04-20 13:12:19
+  from 'cms_template:1' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.2',
+  'unifunc' => 'content_6804d6936ce1c9_78512321',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '50470a741f99fb273c021a465e5d45647bca304c' => 
+    array (
+      0 => 'cms_template:1',
+      1 => '1745146236',
+      2 => 'cms_template',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6804d6936ce1c9_78512321 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_checkPlugins(array(0=>array('file'=>'/Applications/XAMPP/xamppfiles/htdocs/cmsms/lib/plugins/function.global_content.php','function'=>'smarty_function_global_content',),));
+?>
+<!DOCTYPE HTML>
+<!--
+	Introspect by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+--><html>
+<head>
+<title>Introspect by TEMPLATED</title><meta charset="utf-8"><meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"><meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<link rel="stylesheet" href="/cmsms/template/assets/css/main.css"></head><body>
+
+		<!-- Header -->
+			<?php echo smarty_function_global_content(array('name'=>"Template-Header"),$_smarty_tpl);?>
+
+
+		<!-- Banner -->
+			<?php echo smarty_function_global_content(array('name'=>"Template-Banner"),$_smarty_tpl);?>
+
+          
+                  <!-- One -->
+                       <section id="one">
+                             <div class="inner">
+                                
+                             </div>
+                       </section>
+ 
+                    <!-- Two -->
+                         <?php echo smarty_function_global_content(array('name'=>"Template-Two"),$_smarty_tpl);?>
+
+			
+		      <!-- Footer -->
+			 <?php echo smarty_function_global_content(array('name'=>"Template-Footer"),$_smarty_tpl);?>
+
+
+		<!-- Scripts -->
+			<?php echo '<script'; ?>
+ src="/cmsms/template/assets/js/jquery.min.js"><?php echo '</script'; ?>
+>
+                         <?php echo '<script'; ?>
+ src="/cmsms/template/assets/js/skel.min.js"><?php echo '</script'; ?>
+>
+                         <?php echo '<script'; ?>
+ src="/cmsms/template/assets/js/util.js"><?php echo '</script'; ?>
+> 
+                        <?php echo '<script'; ?>
+ src="/cmsms/template/assets/js/main.js"><?php echo '</script'; ?>
+>
+                     </body>
+                   </html><?php }
+}
