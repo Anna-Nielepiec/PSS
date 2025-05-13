7bd29b8b3f6f647021b7f621b8a682aaa478eea1
Anna-Nielepiec <120506261+Anna-Nielepiec@users.noreply.github.com>
2025-05-13 20:21:20 +0200

    Add files via upload

---
+++ cmsms_aktualnosci_i_galeria/tmp/templates_c/cmsms_parser_0_63196600_1747142293^a6a0ae302a71a0e494b8f7be2389793289403d68_1.cms_template.33.php
@@ -0,0 +1,44 @@
+<?php
+/* Smarty version 4.5.2, created on 2025-05-13 15:18:13
+  from 'cms_template:33' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.2',
+  'unifunc' => 'content_682346959dd184_84843826',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'a6a0ae302a71a0e494b8f7be2389793289403d68' => 
+    array (
+      0 => 'cms_template:33',
+      1 => '1747083462',
+      2 => 'cms_template',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_682346959dd184_84843826 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_checkPlugins(array(0=>array('file'=>'/Applications/XAMPP/xamppfiles/htdocs/cmsms/lib/plugins/function.global_content.php','function'=>'smarty_function_global_content',),));
+?>
+<link rel="stylesheet" href="/cmsms/html5up-multiverse/assets/css/fontawesome.css" />
+    <link rel="stylesheet" href="/cmsms/html5up-multiverse/assets/css/main.css" />
+    <link rel="stylesheet" href="/cmsms/template/assets/css/main.css">
+    
+    
+
+
+
+<?php echo smarty_function_global_content(array('name'=>"Template-Header"),$_smarty_tpl);?>
+
+ 
+<div id="main">
+<div class="inner">
+        
+</div>
+</div>
+<?php echo smarty_function_global_content(array('name'=>"Template-Footer_down"),$_smarty_tpl);
+}
+}
