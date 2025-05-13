47ba08a2cf599df54ac854af5ccc0e25fb9009e2
Anna-Nielepiec <120506261+Anna-Nielepiec@users.noreply.github.com>
2025-05-13 20:11:35 +0200

    Add files via upload

---
+++ cmsms_aktualnosci_i_galeria/tmp/templates_c/cmsms_parser_0_04791100_1746646201^42a895c0c0b4af16236eece14768d2e412539c8e_1.cms_template.30.php
@@ -0,0 +1,25 @@
+<?php
+/* Smarty version 4.5.2, created on 2025-05-07 21:30:01
+  from 'cms_template:30' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.2',
+  'unifunc' => 'content_681bb4b90bb748_77930327',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '42a895c0c0b4af16236eece14768d2e412539c8e' => 
+    array (
+      0 => 'cms_template:30',
+      1 => '1746646152',
+      2 => 'cms_template',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_681bb4b90bb748_77930327 (Smarty_Internal_Template $_smarty_tpl) {
+}
+}
