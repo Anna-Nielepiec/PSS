58dc02febad6472fa308df8b6ad7824f1a9a9ea6
Anna-Nielepiec <120506261+Anna-Nielepiec@users.noreply.github.com>
2025-05-13 20:26:27 +0200

    Add files via upload

---
+++ cmsms_aktualnosci_i_galeria/tmp/templates_c/cmsms_parser_0_90363700_1747083570^42a895c0c0b4af16236eece14768d2e412539c8e_1.cms_template.30.php
@@ -0,0 +1,91 @@
+<?php
+/* Smarty version 4.5.2, created on 2025-05-12 22:59:30
+  from 'cms_template:30' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.2',
+  'unifunc' => 'content_68226132e0bf52_33769174',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '42a895c0c0b4af16236eece14768d2e412539c8e' => 
+    array (
+      0 => 'cms_template:30',
+      1 => '1746880671',
+      2 => 'cms_template',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_68226132e0bf52_33769174 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_checkPlugins(array(0=>array('file'=>'/Applications/XAMPP/xamppfiles/htdocs/cmsms/lib/plugins/function.title.php','function'=>'smarty_function_title',),1=>array('file'=>'/Applications/XAMPP/xamppfiles/htdocs/cmsms/lib/plugins/function.sitename.php','function'=>'smarty_function_sitename',),2=>array('file'=>'/Applications/XAMPP/xamppfiles/htdocs/cmsms/lib/plugins/function.global_content.php','function'=>'smarty_function_global_content',),));
+?>
+<!DOCTYPE HTML>
+<!--
+    Generic - Introspect by TEMPLATED
+    templated.co @templatedco
+    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+  <head>
+    <title><?php echo smarty_function_title(array(),$_smarty_tpl);?>
+ – <?php echo smarty_function_sitename(array(),$_smarty_tpl);?>
+</title>
+
+    <meta charset="utf-8">
+    <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="/cmsms/html5up-multiverse/assets/css/main.css" />
+    <link rel="stylesheet" href="/cmsms/template/assets/css/main.css">
+    
+  </head>
+  <body>
+
+    <!-- Header -->
+  <?php echo smarty_function_global_content(array('name'=>"Template-Header"),$_smarty_tpl);?>
+
+
+  
+
+  <header id="header">
+    <h1><?php echo smarty_function_sitename(array(),$_smarty_tpl);?>
+</h1>
+  </header>
+
+  <nav id="menu">
+    
+  </nav>
+
+  <section id="content">
+    <h1><?php echo smarty_function_title(array(),$_smarty_tpl);?>
+</h1>
+    
+  </section>
+
+  <!-- Footer -->
+  <?php echo smarty_function_global_content(array('name'=>"Template-Footer_down"),$_smarty_tpl);?>
+
+
+
+   
+
+    <!-- Scripts -->
+    <?php echo '<script'; ?>
+ src="/cmsms/template/assets/js/jquery.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="/cmsms/template/assets/js/skel.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="/cmsms/template/assets/js/util.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="/cmsms/template/assets/js/main.js"><?php echo '</script'; ?>
+>
+
+  </body>
+</html><?php }
+}
---
+++ cmsms_aktualnosci_i_galeria/tmp/templates_c/cmsms_parser_0_91306000_1747148697^66a7a2e7316b891573c183156aa773829e1bcc80_1.cms_template.32.php
@@ -0,0 +1,47 @@
+<?php
+/* Smarty version 4.5.2, created on 2025-05-13 17:04:57
+  from 'cms_template:32' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.2',
+  'unifunc' => 'content_68235f99e26bb5_96816069',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '66a7a2e7316b891573c183156aa773829e1bcc80' => 
+    array (
+      0 => 'cms_template:32',
+      1 => '1747148127',
+      2 => 'cms_template',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_68235f99e26bb5_96816069 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_checkPlugins(array(0=>array('file'=>'/Applications/XAMPP/xamppfiles/htdocs/cmsms/lib/plugins/function.global_content.php','function'=>'smarty_function_global_content',),));
+?>
+<!-- Gallery Styles and Scripts -->
+ 
+ <link rel="stylesheet" href="/cmsms/html5up-multiverse/assets/css/fontawesome.css" />
+    <link rel="stylesheet" href="/cmsms/html5up-multiverse/assets/css/main.css" />
+    <link rel="stylesheet" href="/cmsms/template/assets/css/main.css">
+    
+    
+
+
+
+<?php echo smarty_function_global_content(array('name'=>"Template-Header"),$_smarty_tpl);?>
+
+ 
+<div id="main">
+<div class="inner">
+        
+        
+</div>
+</div>
+<?php echo smarty_function_global_content(array('name'=>"Template-Footer_down"),$_smarty_tpl);
+}
+}
